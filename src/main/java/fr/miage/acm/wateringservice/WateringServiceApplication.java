a6250684fd802a00d2aef10e56feb11dc75bfa94
Yohan Lussiez <yohan.lussiez@outlook.fr>
2024-06-04 14:33:03 +0200

    First commit

---
+++ src/main/java/fr/miage/acm/wateringservice/WateringServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.acm.wateringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WateringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/acm/wateringservice/WateringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.acm.wateringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WateringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
